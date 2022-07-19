27a00d9bd6be103b110f630487014995ff1bb53c
Michał Śledź <michalsledz34@gmail.com>
2022-07-19 15:13:42 +0200

    Try to enforce 1280x720

--- assets/src/consts.ts
+++ assets/src/consts.ts
@@ -7,10 +7,9 @@ export const AUDIO_TRACK_CONSTRAINTS: MediaTrackConstraints = {
 };
 
 export const VIDEO_TRACK_CONSTRAINTS: MediaTrackConstraints = {
-  width: 1280, height: 720, frameRate: 24,
-  // width: { max: 1280, ideal: 1280, min: 640 },
-  // height: { max: 720, ideal: 720, min: 320 },
-  // frameRate: { max: 30, ideal: 24 },
+  width: { max: 1280, ideal: 1280, min: 1280 },
+  height: { max: 720, ideal: 720, min: 720 },
+  frameRate: { max: 30, ideal: 24 },
 };
 
 export const SCREENSHARING_MEDIA_CONSTRAINTS: DisplayMediaStreamConstraints = {
